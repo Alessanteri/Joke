e6ff1a0343bad75998a268c5e3226cb8a8f1c3ef
Alessanteri <alessanterio@mail.ru>
2021-10-11 16:27:55 +0300

    Adding functions Joke.get_joke/0 and Joke.print_joke/0

---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule Joke.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :joke,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      xref: [exclude: [Jason]]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      applications: [:httpoison]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix, "~> 1.1 or ~> 1.2"},
+      {:phoenix_html, "~> 2.3"},
+      {:cowboy, "~> 1.0", only: [:dev, :test]},
+      {:slime, "~> 0.14"},
+      {:jason, "~> 1.2"},
+      {:httpoison, "~> 1.8"}
+    ]
+  end
+end
